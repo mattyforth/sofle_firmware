076312919834a1d339c92fd877fa32755ab5fe3c
Matthew Forth <matty@studiorepublic.com>
2025-03-20 10:34:23 +0000

    chore: increased tap term slightly

--- config.h
+++ config.h
@@ -15,28 +15,17 @@
  */
 #pragma once
 
-// Enabling this option changes the startup behavior to listen for an
-// active USB communication to delegate which part is master and which
-// is slave. With this option enabled and theres’s USB communication,
-// then that half assumes it is the master, otherwise it assumes it
-// is the slave.
-//
-// I've found this helps with some ProMicros where the slave does not boot
 #define SPLIT_USB_DETECT
-
-/*#define RGB_MATRIX_SLEEP     // turn off effects when suspended*/
-/*#define SPLIT_TRANSPORT_MIRROR             // If LED_MATRIX_KEYPRESSES or LED_MATRIX_KEYRELEASES is enabled, you also will want to enable SPLIT_TRANSPORT_MIRROR*/
-/*#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100  // limits maximum brightness of LEDs (max 255). Higher may cause the controller to crash.*/
 
 #define FORCE_NKRO
 
 #define USB_SUSPEND_WAKEUP_DELAY 200
 
-
-/*#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD*/
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
-#define TAPPING_TERM 150
+/*#define PERMISSIVE_HOLD*/
+/*#define HOLD_ON_OTHER_KEY_PRESS*/
+#define TAPPING_TERM 200
 
-// Experiment with a quick tap term of 175 because I do like to spam jk in vim (was 0 to disable repeat)
+// Allow transitioning quickly from tap to hold
 #define QUICK_TAP_TERM 0
